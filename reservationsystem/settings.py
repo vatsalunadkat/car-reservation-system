ef6b5094c1aedee2d10cbff8a3b209d779ddf29a
Vatsal Unadkat <vatoobhai@gmail.com>
2022-09-02 04:50:22 +0530

    Vehicle DB setup

--- reservationsystem/settings.py
+++ reservationsystem/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'user_register',
+    'vehicle_register',
 ]
 
 MIDDLEWARE = [
--- reservationsystem/urls.py
+++ reservationsystem/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/user/', include('user_register.urls')),
+    path('api/v1/vehicle/', include('vehicle_register.urls')),
 ]
