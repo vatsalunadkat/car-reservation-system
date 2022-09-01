23de66f583ed0951d6928b7bc0b330326dc85bfa
Vatsal Unadkat <vatoobhai@gmail.com>
2022-09-01 18:09:37 +0530

    Feature - Password Hashing

--- user_register/models.py
+++ user_register/models.py
@@ -3,14 +3,15 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+
 class User(AbstractUser):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    email_id = models.CharField(max_length=255, unique=True)
+    email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=225)
-    mobile = models.CharField(max_length = 13)
+    mobile = models.CharField(max_length=13)
     # username is required by AbstractUser, but we can override it.
     username = None
 
-    USERNAME_FIELD = 'email_id'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
--- user_register/serializers.py
+++ user_register/serializers.py
@@ -6,4 +6,17 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'first_name', 'last_name',
-                  'email_id', 'password', 'mobile']
+                  'email', 'password', 'mobile']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+
+        if password is not None:
+            instance.set_password(password)
+
+        instance.save()
+        return instance
