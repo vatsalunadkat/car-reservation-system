04e5fffc31d7b7bb623ffed60c25655ec8f5f31b
Vatsal Unadkat <vatoobhai@gmail.com>
2022-09-01 17:19:21 +0530

    Feature - User Register

--- user_register/models.py
+++ user_register/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_id = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=225)
+    mobile = models.CharField(max_length = 13)
+    # username is required by AbstractUser, but we can override it.
+    username = None
+
+    USERNAME_FIELD = 'email_id'
+    REQUIRED_FIELDS = []
